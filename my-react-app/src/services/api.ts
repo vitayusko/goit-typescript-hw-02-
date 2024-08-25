6808f24e5819a105e06b5be16d1ff84123091675
vitayusko <vitayusko@users.noreply.github.com>
2024-08-25 20:18:50 +0200

    src

--- my-react-app/src/services/api.ts
+++ my-react-app/src/services/api.ts
@@ -1,17 +1,17 @@
-// import axios from "axios";
+import axios from "axios";
 
-// // парсим данные м ансплэша шаг 1
+// парсим данные м ансплэша шаг 1
 
-// export const fetchImages = async (query, perPage = 9, page = 1) => {
-//   const response = await axios.get("https://api.unsplash.com/search/photos", {
-//     params: {
-//       query,
-//       per_page: perPage,
-//       page,
-//     },
-//     headers: {
-//       Authorization: "Client-ID OY7E_vy_zvJ3W0dB39G5FF10cnIMiqwZ6msbbNgPcuk",
-//     },
-//   });
-//   return response.data;
-// };
+export const fetchImages = async (query, perPage = 9, page = 1) => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    params: {
+      query,
+      per_page: perPage,
+      page,
+    },
+    headers: {
+      Authorization: "Client-ID OY7E_vy_zvJ3W0dB39G5FF10cnIMiqwZ6msbbNgPcuk",
+    },
+  });
+  return response.data;
+};
